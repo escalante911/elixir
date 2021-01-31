834112d4d9e319d4217e9d5be01665e9ed3d9028
Jose Luis Escalante <38319711+escalante911@users.noreply.github.com>
2021-01-31 14:50:35 -0500

    Jose

--- basic_types.ex
+++ basic_types.ex
@@ -1,7 +1,30 @@
-# Integers
-n= 102
+# Integer
+n = 102
 n_hex = 0x41 # = 65
 n_octal = 0o24 # = 20
+
+IO.puts("a = #{n}, b = #{n_hex}, c =#{n_octal}")
+
+q = div(35, 4) # 8
+r = rem(35, 4) # 3
+
+# Float
+x = 2.71
+IO.puts("round(#{x}) = #{round(x)}")
+IO.puts("trunc(#{x}) = #{trunc(x)}")
+
+x = 276.5e-2
+IO.puts("round(#{x}) = #{round(x)}")
+IO.puts("trunc(#{x}) = #{trunc(x)}")
+
+isOpen = true # false
+text = "Hello world (#{isOpen})"
+list = [1, "hola", 2.3, false]
+tuple = {1, "2", 3.0}
+atom = :my_atom
+atom2 = MyAtom
+
+
 
 
 
