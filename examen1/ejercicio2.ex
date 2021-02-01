9a25c3defe9c10bc8355090d5bb35110cb789352
Jose Luis Escalante <38319711+escalante911@users.noreply.github.com>
2021-02-01 17:04:15 -0500

    Jose

--- examen1/ejercicio2.ex
+++ examen1/ejercicio2.ex
@@ -1,6 +1,7 @@
 defmodule Evaluacion do
  # Pregunta 2
- def sumr(list) when is_list(list), do: reduce(tl(list), hd(list), &(&1 + &2), [hd(list)])
+ def sumr(list) when is_list(list) and length(list) > 1, do: reduce(tl(list), hd(list), &(&1 + &2), [hd(list)])
+ def sumr(list) when is_list(list) and length(list) <= 1, do: false
 
  def reduce(list, acc, action, lista) when is_list(list) and is_function(action, 2) do
    reducep(list, acc, action, lista)
@@ -10,4 +11,7 @@ defmodule Evaluacion do
  defp reducep([h | t], acc, action, lista), do: reducep(t, action.(h, acc), action, lista ++ [action.(h, acc)])
 end
 
+IO.inspect Evaluacion.sumr([])
 IO.inspect Evaluacion.sumr([1, 3, 4, -1])
+IO.inspect Evaluacion.sumr([1, 3, 4, 2, 5, 6])
+IO.inspect Evaluacion.sumr([1])
