683f08b8707d4b24a1c2ec1f08abd17dd9c59b57
Jose Luis Escalante <38319711+escalante911@users.noreply.github.com>
2021-01-31 20:41:23 -0500

    Jose

--- map_reduce.ex
+++ map_reduce.ex
@@ -1,22 +1,22 @@
 defmodule MyMapReduce do
   #def double(list) when is_list(list), do: doublep(list, [])
   def double(list) when is_list(list), do: map(list, &(2 * &1))
-  defp doublep([], acc), do: acc
-  defp doublep([h | t], acc), do: doublep(t, acc ++ [2 * h])
+  def sum(list) when is_list(list), do: reduce(list, 0, &(&1 + &2))
 
   def map(list, transform) when is_list(list) and is_function(transform, 1) do
-      mapp(list, [], transform)
+      #mapp(list, [], transform)
+      reduce(list, [, fn h, acc -> acc ++ [transform.(h)] end)
   end
 
   defp mapp([], acc, _), do: acc
   defp mapp([h | t], acc, transform), do: mapp(t, acc ++ [transform.(h)], transform)
 
   def reduce(list, acc, action) when is_list(list) and is_function(action, 2) do
-     reducep(list, acc, action)
+    reducep(list, acc, action)
   end
 
   defp reducep([], acc, _), do: acc
-  defp reducep([h | t], acc, action), do: mapp(t, action.(h, acc), action)
+  defp reducep([h | t], acc, action), do: reducep(t, action.(h, acc), action)
 
 end
 
