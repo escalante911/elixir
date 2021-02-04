0b7ca4489f354980b0197a43da804408289230d7
Jose Luis Escalante <38319711+escalante911@users.noreply.github.com>
2021-02-03 22:45:20 -0500

    Jose

---
+++ java/programa.java
@@ -0,0 +1,8 @@
+public class Programa{
+
+    public static void Main(String[] args){
+
+        Singleton single = new Singleton();
+        System.out.printl("Hola");
+    }
+}
--- java/singleton.java
+++ java/singleton.java
@@ -1,9 +1,26 @@
 public class Singleton{
 
-      public static void Main(String[] args) {
+      // Aqui guardamos la unica instancia que va a existir 
+      private static Singleton instancia;
 
-        
-          
+      // Datos Propios de la clase
+      private string nombre;
+      private int edad;
+
+      // Constructor privado
+      private Singleton(){
+            nombre = "";
+            edad = 0;
       }
+
+      public static Singleton ObtenerInstancia(){
+
+            // Verificamos si no existe la instancia 
+            if(instancia == null){
+
+            }
+      }
+
+      
 
 }
