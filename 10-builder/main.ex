83e5b48808f6428898d460c2c936a429086c9f88
Jose Luis Escalante <38319711+escalante911@users.noreply.github.com>
2021-02-08 11:08:27 -0500

    Jose

--- 10-builder/main.ex
+++ 10-builder/main.ex
@@ -3,7 +3,15 @@ import User
 user =
   create_user()
   |> set_name("Jose", "Escalante")
-  |> set_country("Peru")
+  |> set_country("Colombia")
   |> set_languages(["spanish"])
 
   inspect(user) |> IO.puts()
+
+  user =
+    create_user()
+    |> set_name("Frank", "Moreno")
+    |> set_country("Peru")
+    |> set_languages(["spanish", "english", "german"])
+
+    inspect(user) |> IO.puts()
