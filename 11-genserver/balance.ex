f38c7f8d26a4e322414f24db70ae3ceb4c67147f
Jose Luis Escalante <38319711+escalante911@users.noreply.github.com>
2021-02-08 12:14:21 -0500

    Jose

--- 11-genserver/balance.ex
+++ 11-genserver/balance.ex
@@ -15,7 +15,31 @@ defmodule Balance do
   end
 
   def call(balance_pid, event) do
+    send(balance_pid, event)
 
+    receive do
+      amount -> amount
+    end
   end
 
+  def listen(state) do
+    receive do
+      {cmd, amount, from_id} -> {cmd, amount} |> handle_call(from_id, state) |> listen()
+    end
+  end
+
+  def handle_call({:deposit, amount}, from_id, state) do
+    state = state + amount
+    reply(from_id, state, state)
+  end
+
+  def handle_call({:withdraw, amount}, from_id, state) do
+    state = state - amount
+    reply(from_id, state, state)
+  end
+
+  def reply(from_id, reply, new_state) do
+    send(from_id, reply)
+    new_state
+  end
 end
