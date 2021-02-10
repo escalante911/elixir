3cbf40f919a06870e4cb69a87465279246480872
Jose Luis Escalante <38319711+escalante911@users.noreply.github.com>
2021-02-09 20:05:13 -0500

    Jose

--- kv_ets.ex
+++ kv_ets.ex
@@ -1,17 +1,26 @@
 defmodule KvEts do
   use ETS.Utils
 
+  @initial_state {:hello, "world"}
+
   def star_link do
-    :ets.new(:example, [:set, :protected])
+    table = :ets.new(:example, [:set, :protected])
+    :ets.inspect(table, @initial_state)
+    table
   end
 
-  def read(rq, key) do
-
+  def read(ref, key) do
+     [{_, value}] = :ets.lookup(ref, key)
   end
 
   def insert(rq, key, value) do
     :ets.insert(rq, {key, value})
+    ref
   end
 
-
+  def actualizar(ref, key, value) do
+    :ets.insert(ref, {key, value})
+  end
 end
+
+ets = kv_ets.star_link()
