dc389b057e8baee378cb4240d8901551c9acc804
Jose Luis Escalante <38319711+escalante911@users.noreply.github.com>
2021-03-02 20:23:59 -0500

    Jose

--- phoenix_prueba/lib/phoenix_prueba_web/controllers/hello_controller.ex
+++ phoenix_prueba/lib/phoenix_prueba_web/controllers/hello_controller.ex
@@ -1,11 +1,22 @@
 defmodule PhoenixPruebaWeb.HelloController do
   use PhoenixPruebaWeb, :controller
 
+  plug PhoenixPruebaWeb.Plugs.Locale, "es"
+
   def index(conn, _params) do
     render(conn, "index.html")
   end
 
+#  def show(conn, %{"messenger" => messenger}) do
+#    render(conn, "show.html", messenger: messenger)
+#  end
+
   def show(conn, %{"messenger" => messenger}) do
-    render(conn, "show.html", messenger: messenger)
+    text(conn, "From messenger #{messenger}")
   end
+
+  def test(conn, _params) do
+    render(conn, "index.html")
+  end
+
 end
--- phoenix_prueba/lib/phoenix_prueba_web/router.ex
+++ phoenix_prueba/lib/phoenix_prueba_web/router.ex
@@ -7,6 +7,13 @@ defmodule PhoenixPruebaWeb.Router do
     plug :fetch_flash
     plug :protect_from_forgery
     plug :put_secure_browser_headers
+    plug PhoenixPruebaWeb.Plugs.Locale, "es"
+  end
+
+  pipeline :review_checks do
+    plug :browser
+#    plug :ensure_authenticated_user
+#    plug :ensure_user_owns_review
   end
 
   pipeline :api do
@@ -18,8 +25,63 @@ defmodule PhoenixPruebaWeb.Router do
 
     get "/", PageController, :index
     get "/hello", HelloController, :index
-    get "/hello/:messenger", HelloController, :show
+#    get "/hello/:messenger", HelloController, :show
+#    get "/", PageController, :test
+
+#    resources "/users", UserController
+#    resources "/hello", HelloController, only: [:index, :show]
+#    resources "/posts", PostController, only: [:index, :show]
+#    resources "/comments", CommentController, except: [:delete]
+#    resources "/reviews", ReviewController
   end
+
+    resources "/users", UserController do
+    resources "/posts", PostController
+  end
+
+  scope "/admin", PhoenixPruebaWeb.Admin, as: :admin do
+    pipe_through :browser
+
+    resources "/images",  ImageController
+    resources "/reviews", ReviewController
+    resources "/users",   UserController
+  end
+
+#  scope "/api", PhoenixPruebaWeb.Api, as: :api do
+#    pipe_through :api
+#
+#    scope "/v1", V1, as: :v1 do
+#      resources "/images",  ImageController
+#      resources "/reviews", ReviewController
+#      resources "/users",   UserController
+#    end
+#  end
+
+  scope "/", PhoenixPruebaWeb do
+    pipe_through :browser
+
+    resources "/users", UserController
+  end
+
+  scope "/", AnotherAppWeb do
+    pipe_through :browser
+
+    resources "/posts", PostController
+  end
+
+  scope "/reviews", PhoenixPruebaWeb do
+    pipe_through [:browser, :review_checks]
+
+    resources "/", ReviewController
+  end
+
+  forward "/jobs", BackgroundJob.Plug
+
+#  scope "/" do
+#    pipe_through [:authenticate_user, :ensure_admin]
+#    forward "/jobs", BackgroundJob.Plug
+#    forward "/jobs", BackgroundJob.Plug, name: "PhoenixPrueba Phoenix"
+#  end
 
   # Other scopes may use custom stacks.
   # scope "/api", PhoenixPruebaWeb do
